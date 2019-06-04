6b076f6ded705e0f38b78351e8f3fd0feead1fde
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-03 21:48:12 -0700

    rendered profile and signed out button

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,17 +1,27 @@
 import Component from '../Component.js';
-// import { auth } from '../services/firebase.js';
+import Profile from './Profile.js';
+import { auth } from '../services/firebase.js';
 
 class Header extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const profile = new Profile();
+        dom.appendChild(profile.render());
+
+        auth.onAuthStateChanged(user => {
+            profile.update({ user });
+        });
+
+        return dom;
+    }
     renderTemplate() {
         const title = this.props.title || document.title;
         return /*html*/ `
             <header>
                 <h1>${title}</h1>
-                <div class ="profile">
-                    <img src="">
-                </div>
             </header>      
     `;
+
     }
 }
 
