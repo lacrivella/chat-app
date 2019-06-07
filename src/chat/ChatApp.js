e957569c58cf73d46c1ab70a051c44e9e9c1bd14
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-07 09:09:08 -0700

    refactored level of tree data

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -2,23 +2,45 @@ import Component from '../Component.js';
 import Header from '../shared/Header.js';
 import MakeMessage from './MakeMessage.js';
 import QUERY from '../QUERY.js';
-import { roomsRef } from '../services/firebase.js';
+import { roomsRef, messagesByRoomRef } from '../services/firebase.js';
+import MessageList from './MessageList.js';
 
 class ChatApp extends Component {
     render() {
         const dom = this.renderDOM();
 
-        const header = new Header({ title: 'ChatRoom' });
+        const header = new Header();
         const headerDOM = header.render();
 
         const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
 
         const searchParams = QUERY.parse(window.location.search.slice(1));
+        const messagesByRooms = messagesByRoomRef.child(searchParams.key);
         const roomRef = roomsRef.child(searchParams.key);
 
-        const makeMessage = new MakeMessage({ roomRef });
+        messagesByRooms
+            .on('value', snapshot => {
+                const value = snapshot.val();
+                const messages = value ? Object.values(value) : [];
+                messageList.update({ messages: messages });
+            });
+            
+        roomRef
+            .on('value', snapshot => {
+                const value = snapshot.val();
+                header.update({ title: value.name });
+            });
+            
+        const makeMessage = new MakeMessage({ 
+            messagesByRooms,
+            key: searchParams.key
+        });
+
         main.appendChild(makeMessage.render());
+
+        const messageList = new MessageList({ messages: [] });
+        main.appendChild(messageList.render());
 
         return dom;
     }
